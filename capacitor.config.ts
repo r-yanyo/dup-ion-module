d1f375990584e24d38d9d2788ef553a492365334
Ryosuke Homma <r.honma@twogate.com>
2024-02-08 16:12:09 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dup-ion-module',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
